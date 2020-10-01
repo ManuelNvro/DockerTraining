849ba7e7e2aa20672272dc525aff02b1f82ff056
ManuelNvro <manosnavarro@gmail.com>
2020-10-01 10:47:49 -0500

    Hello World Python

---
+++ CI/helloworld.py
@@ -0,0 +1,26 @@
+from OMPython import OMCSessionZMQ
+omc = OMCSessionZMQ()
+from modelicares import SimRes
+import pandas as pd
+import numpy as np
+import os
+import shutil
+
+
+# In[11]:
+
+
+#By default, the code runs in manuelnvro OM 16. To change the computer change the following folders.
+#OpenIPSL Location
+OpenIPSL = "/home/OpenIPSL-master/"
+#GitHub Location
+GitHubOpenIPSL = "https://github.com/marcelofcastro/OpenIPSL.git"
+OpenIPSLPackage = "/home/OpenIPSL-master/OpenIPSL/package.mo"
+#Working Directory
+WorkingDir = "/home/CI/WorkingDir/"
+
+
+print("hello World")
+
+print("we are getting somewhere")
+
